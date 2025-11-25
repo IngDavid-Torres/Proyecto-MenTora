3e20f1544b59d05c303a8930354a38a40fbd56c2
IngDavid-Torres <doser97.mix@outlook.com>
2025-11-25 17:12:10 -0600

    Permitir email nulo en modelo User

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
     password = db.Column(db.String(200), nullable=False)
     area = db.Column(db.String(50)) 
     points = db.Column(db.Integer, default=0)
